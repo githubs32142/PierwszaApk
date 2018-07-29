c048ce967c896717de959581ce6f58cf573bf509
Admin <Admin@DESKTOP-1O7PIA8>
2018-07-29 17:32:22 +0200

    Wersja 1

---
+++ src/test/java/pl/techstyle/Aplikacja1ApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.techstyle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Aplikacja1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
